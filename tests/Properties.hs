eb2de4ef1c778e7b0b3294b5ac16e689c4154d7e
Bryan O'Sullivan <bos@serpentine.com>
2013-10-28 12:34:08 -0700

    Added all the case conversion tests

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -6,15 +6,26 @@ import Data.Text (Text)
 import QuickCheckUtils ()
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
+import qualified Data.Text as T
 import qualified Data.Text.ICU as I
 
 t_rnf :: (NFData a) => (Text -> a) -> Text -> Bool
 t_rnf f t = rnf (f t) == ()
 
-t_toCaseFold bool = t_rnf $ I.toCaseFold bool
+t_nonEmpty :: (Text -> Text) -> Text -> Bool
+t_nonEmpty f t
+    | T.null t  = T.null ft
+    | otherwise = T.length ft > 0
+  where ft = f t
+
+t_toCaseFold bool = t_nonEmpty $ I.toCaseFold bool
+t_toLower locale = t_nonEmpty $ I.toLower locale
+t_toUpper locale = t_nonEmpty $ I.toUpper locale
 
 tests :: Test
 tests =
   testGroup "Properties" [
-   testProperty "t_toCaseFold" t_toCaseFold
+    testProperty "t_toCaseFold" t_toCaseFold
+  , testProperty "t_toLower" t_toLower
+  , testProperty "t_toUpper" t_toUpper
   ]
--- tests/QuickCheckUtils.hs
+++ tests/QuickCheckUtils.hs
@@ -2,9 +2,14 @@
 
 module QuickCheckUtils () where
 
-import Test.QuickCheck (Arbitrary(..))
+import Data.Text.ICU (LocaleName(..))
+import Test.QuickCheck (Arbitrary(..), elements)
 import qualified Data.Text as T
+import Data.Text.ICU.Break (available)
 
 instance Arbitrary T.Text where
     arbitrary = T.pack `fmap` arbitrary
     shrink = map T.pack . shrink . T.unpack
+
+instance Arbitrary LocaleName where
+    arbitrary = elements (Root:available)
